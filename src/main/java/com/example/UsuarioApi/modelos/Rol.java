13c9f7f498075c1c767ddf98d0bbaf22f12793b7
Fran-mndz <shiro54828@gmail.com>
2025-09-09 09:41:04 -0600

    Codificación de modelos, falta la conexion a la db

--- src/main/java/com/example/UsuarioApi/modelos/Rol.java
+++ src/main/java/com/example/UsuarioApi/modelos/Rol.java
@@ -1,4 +1,58 @@
 package com.example.UsuarioApi.modelos;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Rol")
 public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre del rol es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripción del rol es requerido")
+    private String descripcion;
+
+    @OneToMany(mappedBy = "rol", // "rol" es el nombre del campo en la entidad Usuario que mapea esta relación
+            fetch = FetchType.LAZY // Cuando cargues un Rol, los Usuarios no se cargarán hasta que los pidas.
+            // LAZY es el valor por defecto para OneToMany y es recomendado para rendimiento.
+    )
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/com/example/UsuarioApi/modelos/Usuario.java
+++ src/main/java/com/example/UsuarioApi/modelos/Usuario.java
@@ -1,5 +1,86 @@
 package com.example.UsuarioApi.modelos;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre de usuario es requerido")
+    private String nombre;
+
+    @Email
+    @Column(name = "correo", unique = true, nullable = false)
+    @NotBlank(message = "El correo de usuario es requerido")
+    private String correo;
+
+    @NotBlank(message = "La contraseña es requerida")
+    private String password;
+
+    private int estado;
+
+    @ManyToOne(fetch = FetchType.EAGER) // El rol se carga inmediatamente con el usuario
+    @JoinColumn(name = "idRol", nullable = false)
+    private Rol rol;
+
+    @OneToMany(mappedBy = "usuario", // "usuario" es el nombre del campo en la entidad tarjeta que mapea esta relación
+            fetch = FetchType.LAZY // Cuando cargues un Rol, los Usuarios no se cargarán hasta que los pidas.
+            // LAZY es el valor por defecto para OneToMany y es recomendado para rendimiento.
+    )
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
