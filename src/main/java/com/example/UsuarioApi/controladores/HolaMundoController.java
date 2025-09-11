44b330a6e5ddb70ab061dd7fc4e896b00ffab5d4
karen <karendelcarmen19@outlook.es>
2025-09-11 08:41:33 -0600

    cambios en HolaMundoController

--- src/main/java/com/example/UsuarioApi/controladores/HolaMundoController.java
+++ src/main/java/com/example/UsuarioApi/controladores/HolaMundoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HolaMundoController {
 
-    @GetMapping("/hola")
+    @GetMapping("/")
     public String holaMundo() {
         return "¡Hola Mundo desde Spring Boot! get";
     }
